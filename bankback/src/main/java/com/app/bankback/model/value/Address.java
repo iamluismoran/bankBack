2d71c445548997ff8cbf6879e400cf9d9132f99c
iamluismoran <iamluismoran@gmail.com>
2025-09-09 23:59:19 +0200

    feat(model): add Money and Address classes

--- bankback/src/main/java/com/app/bankback/model/value/Address.java
+++ bankback/src/main/java/com/app/bankback/model/value/Address.java
@@ -1,4 +1,28 @@
 package com.app.bankback.model.value;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Address {
+
+    @Column(name = "street", length = 120, nullable = false)
+    private String street;
+
+    @Column(name = "city", length = 80, nullable = false)
+    private String city;
+
+    @Column(name = "country", length = 30, nullable = false)
+    private String country;
+
+    @Column(name = "postal_code", length = 16, nullable = false)
+    private String postalCode;
 }
--- bankback/src/main/java/com/app/bankback/model/value/Money.java
+++ bankback/src/main/java/com/app/bankback/model/value/Money.java
@@ -1,4 +1,54 @@
 package com.app.bankback.model.value;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Money {
+
+    @Column(precision = 19, scale = 2, nullable = false)
+    private BigDecimal amount;
+
+    public Money(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public static Money of(double value) {
+        return new Money(BigDecimal.valueOf(value));
+
+    }
+
+    // Suma otro Money (devuelve NUEVO Money; no muta this) //
+    public Money add(Money other) {
+        return new Money(this.amount.add(other.amount));
+    }
+
+    // Resta otro Money (devuelve NUEVO Money) //
+
+    public Money subtract(Money other) {
+        return new Money(this.amount.subtract(other.amount));
+    }
+
+    // Multiplica por un factor (intereses/fees) //
+    public Money multiply(BigDecimal factor) {
+        return new Money(this.amount.multiply(factor));
+    }
+
+    // Ajusta a 2 decimales con redondeo bancario //
+    public Money scaled() {
+        return new Money(this.amount.setScale(2, RoundingMode.HALF_EVEN));
+    }
+
+    public boolean lt(Money other) {
+        return this.amount.compareTo(other.amount) < 0;
+    }
 }
