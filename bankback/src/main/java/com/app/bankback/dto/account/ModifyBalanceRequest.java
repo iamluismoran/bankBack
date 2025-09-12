fc74cc9d59e6b622e121ce4e12c0273cad2edb80
iamluismoran <iamluismoran@gmail.com>
2025-09-12 18:00:08 +0200

    feat(dto/account): add ModifyBalanceRequest dto

--- bankback/src/main/java/com/app/bankback/dto/account/ModifyBalanceRequest.java
+++ bankback/src/main/java/com/app/bankback/dto/account/ModifyBalanceRequest.java
@@ -1,4 +1,16 @@
 package com.app.bankback.dto.account;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * Admin: modificar directamente el balance de una cuenta concreta.
+ * Útil para que Admins puedan modificar balances
+ * */
+
+@Data
 public class ModifyBalanceRequest {
+    private Long accountId;
+    private BigDecimal newBalance;
 }
