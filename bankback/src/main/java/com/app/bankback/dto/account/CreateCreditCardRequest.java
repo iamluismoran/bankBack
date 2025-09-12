83ecb4fc94711a1d1fa278ac3d0f95fe82084aaf
iamluismoran <iamluismoran@gmail.com>
2025-09-12 17:40:49 +0200

    feat(dto/account): add CreateCreditCardRequest dto

--- bankback/src/main/java/com/app/bankback/dto/account/CreateCreditCardRequest.java
+++ bankback/src/main/java/com/app/bankback/dto/account/CreateCreditCardRequest.java
@@ -1,4 +1,24 @@
 package com.app.bankback.dto.account;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * Admin: crear una cuenta en Savings
+ * Si minimumBalance o interestRate vienen null, se usarán defaults del service
+ */
+
+@Data
 public class CreateCreditCardRequest {
+    private Long primaryOwnerId;
+    private Long secondaryOwnerId;
+    private BigDecimal initialAmount;
+    private String secretKey;
+
+    /** Mínimo primitido: >= 100. Si null -> default 1000 */
+    private BigDecimal minimumBalance;
+
+    /** Rango: 0.0 < rate <= 0.5. Si null -> default 0.0025 (0.25%)</=> */
+    private BigDecimal interestRate;
 }
